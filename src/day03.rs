11a730c808b321c0586b8d528ed775f28b983ff5
Thomas Whiteway <thomas.whiteway@gmail.com>
2024-12-03 08:18:33 +0000

    Day 3 Part 2

--- src/day03.rs
+++ src/day03.rs
@@ -4,16 +4,27 @@ use regex::Regex;
 
 pub struct Solver {}
 
-fn calculate(data: &str) -> u64 {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+fn calculate(data: &str, always_enabled: bool) -> u64 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
 
     re.captures_iter(data)
-        .map(|m| {
-            let x: u64 = m.get(1).unwrap().as_str().parse().unwrap();
-            let y: u64 = m.get(2).unwrap().as_str().parse().unwrap();
-            x * y
+        .fold((0, true), |(mut total, mut enabled), instruction| {
+            let command = instruction[0].split_once("(").unwrap().0;
+            match command {
+                "mul" => {
+                    if enabled {
+                        let x: u64 = instruction[1].parse().unwrap();
+                        let y: u64 = instruction[2].parse().unwrap();
+                        total += x * y
+                    }
+                }
+                "do" => enabled = true,
+                "don't" => enabled = always_enabled || false,
+                cmd => panic!("Unexpected command: {}", cmd),
+            }
+            (total, enabled)
         })
-        .sum()
+        .0
 }
 
 impl super::Solver for Solver {
@@ -24,8 +35,9 @@ impl super::Solver for Solver {
     }
 
     fn solve(data: Self::Problem) -> (Option<String>, Option<String>) {
-        let part1 = calculate(&data);
+        let part1 = calculate(&data, true);
+        let part2 = calculate(&data, false);
 
-        (Some(part1.to_string()), None)
+        (Some(part1.to_string()), Some(part2.to_string()))
     }
 }
