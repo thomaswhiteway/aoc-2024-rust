7d4f8de7e4163290175c8a26323f4b85a21ec327
Thomas Whiteway <thomas.whiteway@gmail.com>
2024-12-11 08:23:54 +0000

    Day 11 Part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,28 +1,41 @@
+use std::collections::HashMap;
+
 use failure::{err_msg, Error};
+use itertools::Either;
 
-fn count_stones(mut stones: Vec<u64>) -> usize {
-    for _ in 0..25 {
-        let mut new_stones = vec![];
+fn updated_stones(stone: u64) -> impl Iterator<Item = u64> {
+    if stone == 0 {
+        Either::Left([1].into_iter())
+    } else {
+        let width = stone.ilog10() + 1;
+        if width % 2 == 0 {
+            let base = 10u64.pow(width / 2);
+            Either::Right([stone / base, stone % base].into_iter())
+        } else {
+            Either::Left([stone * 2024].into_iter())
+        }
+    }
+}
 
-        for val in stones {
-            if val == 0 {
-                new_stones.push(1);
-            } else {
-                let width = val.ilog10() + 1;
-                if width % 2 == 0 {
-                    let base = 10u64.pow(width / 2);
-                    new_stones.push(val / base);
-                    new_stones.push(val % base);
-                } else {
-                    new_stones.push(val * 2024);
-                }
+fn count_stones(init_stones: Vec<u64>, blinks: usize) -> usize {
+    let mut stones = HashMap::new();
+    for stone in init_stones {
+        *stones.entry(stone).or_default() += 1;
+    }
+
+    for _ in 0..blinks {
+        let mut new_stones = HashMap::new();
+
+        for (stone, count) in stones {
+            for new_stone in updated_stones(stone) {
+                *new_stones.entry(new_stone).or_default() += count;
             }
         }
 
         stones = new_stones;
     }
 
-    stones.len()
+    stones.values().sum()
 }
 
 pub struct Solver {}
@@ -40,7 +53,8 @@ impl super::Solver for Solver {
     }
 
     fn solve(stones: Self::Problem) -> (Option<String>, Option<String>) {
-        let part1 = count_stones(stones);
-        (Some(part1.to_string()), None)
+        let part1 = count_stones(stones.clone(), 25);
+        let part2 = count_stones(stones, 75);
+        (Some(part1.to_string()), Some(part2.to_string()))
     }
 }
