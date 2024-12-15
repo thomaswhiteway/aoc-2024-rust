c6fe962d0848da854672b3a410dea72c181a9554
Thomas Whiteway <thomas.whiteway@gmail.com>
2024-12-15 15:28:38 +0000

    Day 15 Part 2

--- src/day15.rs
+++ src/day15.rs
@@ -3,10 +3,33 @@ use std::collections::HashMap;
 use crate::common::{Direction, Position};
 use failure::{err_msg, Error};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+impl Side {
+    fn other(self) -> Self {
+        match self {
+            Side::Left => Side::Right,
+            Side::Right => Side::Left,
+        }
+    }
+
+    fn as_direction(self) -> Direction {
+        match self {
+            Side::Left => Direction::West,
+            Side::Right => Direction::East,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
 pub enum Object {
     Wall,
-    Box,
+    SmallBox,
+    LargeBox(Side),
 }
 
 #[allow(unused)]
@@ -25,7 +48,9 @@ fn display_map(robot_position: Position, objects: &HashMap<Position, Object>) {
                 } else {
                     match objects.get(&pos) {
                         None => ".",
-                        Some(Object::Box) => "O",
+                        Some(Object::SmallBox) => "O",
+                        Some(Object::LargeBox(Side::Left)) => "[",
+                        Some(Object::LargeBox(Side::Right)) => "]",
                         Some(Object::Wall) => "#",
                     }
                 }
@@ -37,21 +62,41 @@ fn display_map(robot_position: Position, objects: &HashMap<Position, Object>) {
     println!();
 }
 
-fn next_free_position(
+fn find_moved_objects(
     robot_position: Position,
     objects: &HashMap<Position, Object>,
     direction: Direction,
-) -> Option<Position> {
-    (1..)
-        .find_map(|steps| {
-            let this_position = robot_position.step_by(direction, steps);
-            match objects.get(&this_position) {
-                None => Some(Some(this_position)),
-                Some(Object::Wall) => Some(None),
-                Some(Object::Box) => None,
+) -> Option<HashMap<Position, Object>> {
+    let mut to_check = vec![robot_position.step(direction)];
+    let mut moved_objects = HashMap::new();
+
+    while let Some(pos) = to_check.pop() {
+        if moved_objects.contains_key(&pos) {
+            continue;
+        }
+
+        match objects.get(&pos) {
+            None => {}
+            Some(Object::Wall) => {
+                return None;
             }
-        })
-        .unwrap()
+            Some(Object::SmallBox) => {
+                moved_objects.insert(pos, Object::SmallBox);
+                to_check.push(pos.step(direction));
+            }
+            Some(&Object::LargeBox(side)) => {
+                moved_objects.insert(pos, Object::LargeBox(side));
+                to_check.push(pos.step(direction));
+
+                let other_side = side.other();
+                let other_pos = pos.step(other_side.as_direction());
+                moved_objects.insert(other_pos, Object::LargeBox(other_side));
+                to_check.push(other_pos.step(direction));
+            }
+        }
+    }
+
+    Some(moved_objects)
 }
 
 fn perform_move(
@@ -59,12 +104,16 @@ fn perform_move(
     objects: &mut HashMap<Position, Object>,
     direction: Direction,
 ) {
-    if let Some(next_free) = next_free_position(*robot_position, objects, direction) {
-        let next_step = robot_position.step(direction);
-        if let Some(object) = objects.remove(&next_step) {
-            objects.insert(next_free, object);
+    if let Some(to_move) = find_moved_objects(*robot_position, objects, direction) {
+        for (pos, _) in to_move.iter() {
+            objects.remove(pos);
         }
-        *robot_position = next_step;
+
+        for (pos, object) in to_move {
+            objects.insert(pos.step(direction), object);
+        }
+
+        *robot_position = robot_position.step(direction);
     }
 }
 
@@ -87,7 +136,7 @@ fn find_box_location_sum(
     objects
         .iter()
         .filter_map(|(&position, &object)| {
-            if object == Object::Box {
+            if matches!(object, Object::SmallBox | Object::LargeBox(Side::Left)) {
                 Some(position)
             } else {
                 None
@@ -95,6 +144,40 @@ fn find_box_location_sum(
         })
         .map(|pos| 100 * pos.y + pos.x)
         .sum()
+}
+
+fn expand_map(
+    robot_position: Position,
+    objects: &HashMap<Position, Object>,
+) -> (Position, HashMap<Position, Object>) {
+    let new_robot_position = Position {
+        x: robot_position.x * 2,
+        y: robot_position.y,
+    };
+
+    let new_objects = objects
+        .iter()
+        .flat_map(|(pos, obj)| {
+            let new_pos_left = Position {
+                x: pos.x * 2,
+                y: pos.y,
+            };
+            let new_pos_right = Position {
+                x: pos.x * 2 + 1,
+                y: pos.y,
+            };
+
+            [new_pos_left, new_pos_right].into_iter().zip(match obj {
+                Object::Wall => [Object::Wall, Object::Wall],
+                Object::SmallBox => [Object::LargeBox(Side::Left), Object::LargeBox(Side::Right)],
+                Object::LargeBox(_) => {
+                    panic!("Can't expand large box");
+                }
+            })
+        })
+        .collect();
+
+    (new_robot_position, new_objects)
 }
 
 pub struct Solver {}
@@ -121,7 +204,7 @@ impl super::Solver for Solver {
             .flat_map(|(y, line)| {
                 line.char_indices().filter_map(move |(x, c)| match c {
                     '#' => Some(((x, y).into(), Object::Wall)),
-                    'O' => Some(((x, y).into(), Object::Box)),
+                    'O' => Some(((x, y).into(), Object::SmallBox)),
                     _ => None,
                 })
             })
@@ -140,7 +223,11 @@ impl super::Solver for Solver {
     }
 
     fn solve((robot_position, objects, moves): Self::Problem) -> (Option<String>, Option<String>) {
-        let part1 = find_box_location_sum(robot_position, objects, &moves);
-        (Some(part1.to_string()), None)
+        let part1 = find_box_location_sum(robot_position, objects.clone(), &moves);
+
+        let (new_robot_position, expanded_objects) = expand_map(robot_position, &objects);
+        let part2 = find_box_location_sum(new_robot_position, expanded_objects, &moves);
+
+        (Some(part1.to_string()), Some(part2.to_string()))
     }
 }
